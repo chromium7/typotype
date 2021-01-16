379c3773702f44f71ff858db292cf4306675ad1f
Chromium7 <christofersaputra29@gmail.com>
2021-01-16 20:29:32 +0700

    Added plans in comments on model, admin and views

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Activity, Sentence
 
 # Register your models here.
+
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ["user"]
+
+
+@admin.register(Sentence)
+class SentenceAdmin(admin.ModelAdmin):
+    pass
--- game/models.py
+++ game/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+# User (player)
+# User activities -> foreign key user model
+# User cumulative points, rank
+# Game sentences, sentence grade
+
+
+class Activity(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="address")
+
+
+class Sentence(models.Model):
+    text = models.TextField()
+    grade = models.IntegerField()
