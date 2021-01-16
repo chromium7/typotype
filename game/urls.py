a833a29c33e62ee811cefdeeedf5817471635cb8
Chromium7 <christofersaputra29@gmail.com>
2021-01-16 20:12:03 +0700

    Initial commit

---
+++ game/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from . import views
+
+app_name = "game"
+
+urlpatterns = [
+    path("", views.index, name="home"),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("register/", views.register, name="register"),
+
+]
