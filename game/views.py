12c5e06cd7043f4f2bd1ad64fc621374afe94b0d
Chromium7 <christofersaputra29@gmail.com>
2021-01-22 17:23:10 +0700

    Added HTML and CSS files for user interface

--- game/views.py
+++ game/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 
 from .models import Activity, Profile
+from .forms import UserRegistrationForm
 
 
 def index(request):
@@ -11,7 +15,10 @@ def index(request):
         pass
     else:
         login_form = AuthenticationForm()
+        registration_form = UserRegistrationForm()
+
         context['login_form'] = login_form
+        context['registration_form'] = registration_form
 
     if request.session.get('status'):
         context['status'] = request.session['status']
@@ -20,7 +27,22 @@ def index(request):
     return render(request, "game/index.html", context)
 
 
+@require_POST
+def register_text(request):
+    pass
 
+
+@csrf_exempt
+def generate_text(request):
+    return HttpResponse("BRUH", content_type="text/plain")
+
+
+@require_POST
+def submit_activity(request):
+    pass
+
+
+@require_POST
 def login_view(request):
     username = request.POST['username']
     password = request.POST['password']
@@ -33,11 +55,19 @@ def login_view(request):
     return redirect("game:home")
 
 
+@require_POST
 def logout_view(request):
     logout(request)
     return redirect("game:home")
 
 
+@require_POST
 def register(request):
-    
-    return render(request, "game/index.html")
+    form = UserRegistrationForm(request.POST)
+    if form.is_valid():
+        new_user = form.save(commit=False)
+        new_user.set_password(form.cleaned_data['password'])
+        new_user.save()
+    else:
+        request.session['status'] = 'nr'
+    return redirect("game:home")
